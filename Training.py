60371027560a4c71883ccccd70df5f4c440e167b
SamironBiswas <161868541+SamironBiswas@users.noreply.github.com>
2024-12-14 17:11:04 +0530

    Create Training.py

---
+++ Training.py
@@ -0,0 +1,28 @@
+
+from stable_baselines3 import PPO
+import numpy as np
+import gym
+import random
+from env import CustomEnv 
+
+import time 
+import os 
+models_dir = "MODELS/PPO"
+logdir = "logs"
+
+env = CustomEnv()
+if not os.path.exists(models_dir):
+    os.makedirs(models_dir)
+
+if not os.path.exists(logdir):
+    os.makedirs(logdir)
+
+env.reset()
+
+model = PPO("MlpPolicy",env,verbose=1,tensorboard_log=logdir)
+
+TIMESTEPS = 10000
+steps = 300
+for step in range(steps):
+    model.learn(total_timesteps=TIMESTEPS,reset_num_timesteps=False,tb_log_name="PPO")
+    model.save(f"{models_dir}/{TIMESTEPS*step}")
